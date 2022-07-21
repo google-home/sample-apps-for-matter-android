c3e6eadab1e9f0511b83648333de2d9509aac7d7
Vinit Gala <vinitg@google.com>
2022-07-20 21:22:59 -0500

    Enter setup code in test.
    Added functionality to enter the set up code
    and move to the next screen while running the test.
    
    Change-Id: I1b06b5466379604a950812408d383c99a36b5e0a
    Reviewed-on: https://team-review.git.corp.google.com/c/npe-tse-chip-sdk/example-android-matter/+/1521022
    Reviewed-by: Pierre Delisle <pierred@google.com>

--- app/src/androidTest/java/com/google/homesampleapp/DeviceActionTest.kt
+++ app/src/androidTest/java/com/google/homesampleapp/DeviceActionTest.kt
@@ -9,19 +9,29 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiObject
+import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiSelector
-import org.junit.Assert.assertEquals
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiObject2
+import androidx.test.uiautomator.Until
+import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.time.Duration
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class DeviceActionTest {
 
+    private val TEN_SECONDS = Duration.ofSeconds(10).toMillis()
+    private val SCAN_QR_CODE_TITLE = By.text("Scan the QR code")
+    private val TRY_WITH_SETUP_CODE_BUTTON = By.text("Try with setup code")
+    private val ENTER_SETUP_CODE_TITLE = By.textContains("Enter setup code")
+    private val SETUP_CODE_TEXTBOX = UiSelector().className("android.widget.EditText").instance(0)
+    private val SETUP_CODE = "749701123365521327687"
+    private val NEXT_BUTTON = By.text("Next")
     private lateinit var device: UiDevice
 
     @get:Rule
@@ -33,17 +43,33 @@ class DeviceActionTest {
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
     }
 
-    @Test
-    fun addDeviceButton_shouldTriggerScanForQRCode() {
+    fun triggerScanForQRCode() {
         // Verify Add Device button is displayed.
         onView(withId(R.id.addDeviceButton)).check(matches(isDisplayed()))
         // Click the Add Device button.
         onView(withId(R.id.addDeviceButton)).perform(click())
         // Verify the Google play services screen is displayed
         // to scan the QR code.
-        val scanQRCodeItem: UiObject = device.findObject(
-            UiSelector().text("Scan the QR code")
-        )
-        assertEquals(true, scanQRCodeItem.exists());
+        assertNotNull(device.wait(Until.hasObject(SCAN_QR_CODE_TITLE), TEN_SECONDS))
+        assertNotNull(device.wait(Until.hasObject(TRY_WITH_SETUP_CODE_BUTTON), TEN_SECONDS))
+        // Click on Try with setup code option.
+        device.findObject(TRY_WITH_SETUP_CODE_BUTTON).click()
+    }
+
+    fun enterSetupCode() {
+        // Verify the enter setup code screen.
+        assertNotNull(device.wait(Until.hasObject(ENTER_SETUP_CODE_TITLE), TEN_SECONDS))
+        // Enter the setup code.
+        device.findObject(SETUP_CODE_TEXTBOX).setText(SETUP_CODE)
+        // Click the next button
+        val nextButton: UiObject2 = device.findObject(NEXT_BUTTON)
+        assertNotNull(nextButton.wait(Until.clickable(true), TEN_SECONDS))
+        nextButton.click()
+    }
+
+    @Test
+    fun addDevice() {
+        triggerScanForQRCode()
+        enterSetupCode()
     }
 }
