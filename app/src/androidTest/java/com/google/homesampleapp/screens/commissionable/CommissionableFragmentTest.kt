b5990806b37c4c58e5c267ab994f20743cf1fb72
Pierre Delisle <pierred@google.com>
2023-04-28 17:28:23 -0700

    Major cleanup of all our Espresso/UiAutomator tests. (#124)
    CommissionDeviceAndToggleOnOffTest.kt is now a pretty compelling E2E test.

--- app/src/androidTest/java/com/google/homesampleapp/screens/commissionable/CommissionableFragmentTest.kt
+++ app/src/androidTest/java/com/google/homesampleapp/screens/commissionable/CommissionableFragmentTest.kt
@@ -16,17 +16,27 @@
 
 package com.google.homesampleapp.screens.commissionable
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.google.homesampleapp.DeveloperUtilitiesScreen
+import com.google.homesampleapp.HomeScreen
 import com.google.homesampleapp.MainActivity
-import com.google.homesampleapp.R
+import com.google.homesampleapp.SettingsScreen
+import com.google.homesampleapp.screens.commissionable.ble.MatterBeaconProducerBleFake
+import com.google.homesampleapp.screens.commissionable.ble.ModuleBle
+import com.google.homesampleapp.screens.commissionable.mdns.MatterBeaconProducerMdnsFake
+import com.google.homesampleapp.screens.commissionable.wifi.MatterBeaconProducerWifiFake
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoSet
+import javax.inject.Singleton
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -36,17 +46,50 @@ import org.junit.runner.RunWith
  * Test to verify that Matter beacons are properly shown in CommissionableFragment.
  *
  * The test navigates to the "Commissionable devices" screen, and then each beacon producer (BLE,
- * mDNS, Wi-Fi) emits a beacon at a specific interval. Simply visually inspect that all these
- * beacons are properly shown on the screen (e.g. proper icon, inactive mDNS services shows with
- * different color, etc).
+ * mDNS, Wi-Fi) emits a beacon at a specific interval.
+ *
+ * Simply visually inspect that all these beacons are properly shown on the screen (e.g. proper
+ * icon, inactive mDNS services shows with different color, etc).
  */
+
+// See https://developer.android.com/training/dependency-injection/hilt-testing
+@UninstallModules(ModuleBle::class)
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 @HiltAndroidTest
 class CommissionableFragmentTest {
+  @Module
+  @InstallIn(SingletonComponent::class)
+  internal abstract class ModuleBleTest {
+    @Singleton
+    @Binds
+    @IntoSet
+    abstract fun bindsMatterBeaconProducer(
+        fakeBeacon: MatterBeaconProducerBleFake
+    ): MatterBeaconProducer
+  }
 
-  private val DEVELOPER_UTILITIES = "Developer utilities"
-  private val COMMISSIONABLE_DEVICES = "Commissionable devices"
+  @Module
+  @InstallIn(SingletonComponent::class)
+  internal abstract class ModuleMdnsTest {
+    @Singleton
+    @Binds
+    @IntoSet
+    abstract fun bindsMatterBeaconProducer(
+        fakeBeacon: MatterBeaconProducerMdnsFake
+    ): MatterBeaconProducer
+  }
+
+  @Module
+  @InstallIn(SingletonComponent::class)
+  internal abstract class ModuleWifiTest {
+    @Singleton
+    @Binds
+    @IntoSet
+    abstract fun bindsMatterBeaconProducer(
+        fakeBeacon: MatterBeaconProducerWifiFake
+    ): MatterBeaconProducer
+  }
 
   @get:Rule val activityRule = ActivityScenarioRule(MainActivity::class.java)
   @get:Rule var hiltRule = HiltAndroidRule(this)
@@ -58,52 +101,18 @@ class CommissionableFragmentTest {
   }
 
   // ---------------------------------------------------------------------------
-  // Tests support functions
-
-  /**
-   * When the app starts, it may or may not display the codelab dialog depending on the state it was
-   * in the last time it was launched. Was unable to find a way to force the preferences datastore
-   * to be cleared prior to the test being run. Having the code below:
-   * ```
-   * @Before fun init() {
-   *   scope.launch {
-   *     userPreferencesRepository.clearData()
-   *   }
-   * }
-   * ```
-   *
-   * does not complete prior to the start of the test.
-   *
-   * So for now, if we fail clicking on "OK" this means the codelab dialog is not shown and we
-   * simply ignore the exception.
-   *
-   * TODO: If someone knows of a clean way to force a clear of the preferences datastore before the
-   *   test runs, please submit a PR!
-   */
-  private fun clickOkOnCodelabDialog() {
-    try {
-      onView(withText("OK")).perform(click())
-    } catch (e: Throwable) {
-      // The Codelab dialog was not shown. Simply ignore the error.
-      System.out.println("*** Codelab Dialog was not shown.")
-    }
-  }
-
-  // ---------------------------------------------------------------------------
-  // Tests
+  // Test
 
   @Test
   fun testFragmentBehavior() {
-    // Click on "OK" of the codelab dialog.
-    // In its own function because the dialog may, or may not be shown and lots of comments
-    // associated with that :-).
-    clickOkOnCodelabDialog()
-    // Click on "Settings"
-    onView(withId(R.id.settings)).perform(click())
-    // Click on "Developer utilities"
-    onView(ViewMatchers.withText(DEVELOPER_UTILITIES)).perform(click())
-    // Click on "Commissionable devices"
-    onView(ViewMatchers.withText(COMMISSIONABLE_DEVICES)).perform(click())
+    // Handle codelab dialog.
+    HomeScreen.ensureCodelabDialogNotShown()
+
+    // Navigate to "Commissionable devices" screen.
+    HomeScreen.navigateToSettingsScreen()
+    SettingsScreen.selectDeveloperUtilities()
+    DeveloperUtilitiesScreen.selectCommissionableDevices()
+
     // Let the producers run for 60 seconds, giving the user a chance to verify that all beacons
     // are properly shown on screen.
     Thread.sleep(60000)
