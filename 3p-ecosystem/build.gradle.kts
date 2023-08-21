bd576f049ff5acf820377b8fe9b4f0afcdc0b01c
Pierre Delisle <pierred@google.com>
2023-08-21 10:03:02 -0700

    Upgrade to Android Studio Giraffe, as well as to latest plugins and dependencies. (#160)
    "./gradlew build" now works (make sure JAVA_HOME is set properly)
    Migrate to version catalogs.
    See https://developer.android.com/build/migrate-to-catalogs.

--- 3p-ecosystem/build.gradle.kts
+++ 3p-ecosystem/build.gradle.kts
@@ -1,3 +1,15 @@
+// Module-level build file.
+// This file t lets you configure build settings for the specific module it is located in.
+// Configuring these build settings lets you provide custom packaging options,
+// such as additional build types and product flavors, and override settings in the
+// main/ app manifest or top-level build script.
+
+/**
+ * The first section in this file applies among other things the Android Gradle plugin
+ * to this build and makes the android block available to specify
+ * Android-specific build options.
+ */
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -9,21 +21,73 @@ plugins {
     id("com.ncorti.ktfmt.gradle") version "0.12.0"
 }
 
+/**
+ * Locate (and possibly download) a JDK used to build your kotlin
+ * source code. This also acts as a default for sourceCompatibility,
+ * targetCompatibility and jvmTarget. Note that this does not affect which JDK
+ * is used to run the Gradle build itself, and does not need to take into
+ * account the JDK version required by Gradle plugins (such as the
+ * Android Gradle Plugin)
+ */
+kotlin {
+    jvmToolchain(17)
+}
+
+/**
+ * The android block is where you configure all your Android-specific
+ * build options.
+ */
 android {
+    /**
+     * The app's namespace. Used primarily to access app resources.
+     */
     namespace = "com.google.homesampleapp"
+
+    /**
+     * compileSdk specifies the Android API level Gradle should use to
+     * compile your app. This means your app can use the API features included in
+     * this API level and lower.
+     */
     compileSdk = 33
 
+    /**
+     * The defaultConfig block encapsulates default settings and entries for all
+     * build variants and can override some attributes in main/AndroidManifest.xml
+     * dynamically from the build system. You can configure product flavors to override
+     * these values for different versions of your app.
+     */
     defaultConfig {
-        // DeviceSharingClone: Change this value for the target commissioner cloned app
+        // Uniquely identifies the package for publishing.
         applicationId = "com.google.homesampleapp"
+
+        // Defines the minimum API level required to run the app.
         minSdk = 27
+
+        // Specifies the API level used to test the app.
         targetSdk = 33
+
+        // Defines the version number of your app.
         versionCode = 15
+
+        // Defines a user-friendly version name for your app.
         versionName = "1.4.1"
+
+        // Test Runner.
         testInstrumentationRunner = "com.google.homesampleapp.CustomTestRunner"
     }
 
+    /**
+     * The buildTypes block is where you can configure multiple build types.
+     * By default, the build system defines two build types: debug and release. The
+     * debug build type is not explicitly shown in the default build configuration,
+     * but it includes debugging tools and is signed with the debug key. The release
+     * build type applies ProGuard settings and is not signed by default.
+     */
     buildTypes {
+        /**
+         * By default, Android Studio configures the release build type to enable code
+         * shrinking, using minifyEnabled, and specifies the default ProGuard rules file.
+         */
         getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
@@ -32,6 +96,14 @@ android {
         }
     }
 
+    // FIXME
+    /**
+     * To override source and target compatibility (if different from the
+     * toolchain JDK version), add the following. All of these
+     * default to the same value as kotlin.jvmToolchain. If you're using the
+     * same version for these values and kotlin.jvmToolchain, you can
+     * remove these blocks.
+     */
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -49,7 +121,19 @@ android {
     }
     android.buildFeatures.viewBinding = true
 
-    // Specifies one flavor dimension.
+    /**
+     * The productFlavors block is where you can configure multiple product flavors.
+     * This lets you create different versions of your app that can
+     * override the defaultConfig block with their own settings. Product flavors
+     * are optional, and the build system does not create them by default.
+     *
+     * For example, one can create a free and paid product flavor. Each product flavor
+     * then specifies its own application ID, so that they can exist on the Google
+     * Play Store, or an Android device, simultaneously.
+     *
+     * If you declare product flavors, you must also declare flavor dimensions
+     * and assign each flavor to a flavor dimension.
+     */
     flavorDimensions += "version"
     productFlavors {
         create("default") {
@@ -78,59 +162,61 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "third_party/connectedhomeip/libs", "include" to listOf("*.jar", "*.so"))))
 
     // Connected Home
-    implementation("com.google.android.gms:play-services-base:18.2.0")
-    implementation("com.google.android.gms:play-services-home:16.0.0")
+    implementation(libs.play.services.base)
+    implementation(libs.play.services.home)
 
     // AndroidX
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.databinding:databinding-runtime:8.0.2")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.preference:preference:1.2.0")
+    implementation(libs.appcompat)
+    implementation(libs.constraintlayout)
+    implementation(libs.core.ktx)
+    implementation(libs.databinding.runtime)
+    implementation(libs.legacy.support.v4)
+    implementation(libs.preference)
 
     // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
 
     // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation(libs.lifecycle.extensions)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
 
     // Datastore
-    implementation("androidx.datastore:datastore:1.0.0")
-    implementation("androidx.datastore:datastore-core:1.0.0")
-    implementation("com.google.protobuf:protobuf-javalite:3.18.0")
+    implementation(libs.datastore)
+    implementation(libs.datastore.core)
+    implementation(libs.protobuf.javalite)
 
     // Hilt
     // https://dagger.dev/hilt/gradle-setup
-    implementation("com.google.dagger:hilt-android:2.46.1")
-    kapt("com.google.dagger:hilt-compiler:2.46.1")
-    implementation("com.google.ar:core:1.38.0")
+    // TODO: Upgrade to KSP when supported by Hilt/Dagger.
+    //      https://developer.android.com/build/migrate-to-ksp#replace-annotation
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    implementation(libs.core)
 
 
     // Hilt For instrumentation tests
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.44.2")
-    kaptAndroidTest("com.google.dagger:hilt-compiler:2.46.1")
+    androidTestImplementation(libs.hilt.android.testing)
+    kaptAndroidTest(libs.hilt.compiler)
 
     // Task.await()
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+    implementation(libs.kotlinx.coroutines.play.services)
 
     // Other
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation(libs.material)
+    implementation(libs.timber)
 
     // Test
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.espresso.intents)
+    androidTestImplementation(libs.espresso.contrib)
+    androidTestImplementation(libs.runner)
+    androidTestImplementation(libs.rules)
+    androidTestImplementation(libs.uiautomator)
 }
 
 // Issue with androidx.test.espresso:espresso-contrib:3.5.1
@@ -146,7 +232,7 @@ kapt {
 protobuf {
     protoc {
         // For Apple M1 Chip
-        val isMac = System.getProperty("os.name").toLowerCase().contains("mac")
+        val isMac = System.getProperty("os.name").lowercase().contains("mac")
         val protocDepSuffix = if (isMac) ":osx-x86_64" else ""
         artifact = "com.google.protobuf:protoc:3.14.0" + protocDepSuffix
     }
