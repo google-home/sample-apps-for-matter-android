c990118bfc7d42a2dcbe11a8e0536c28bcc8c349
Pierre Delisle <pierred@google.com>
2023-08-21 15:54:29 -0700

    Upgrade kotlin to latest version: 1.9.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,10 @@ plugins {
     id("com.android.library") version "8.1.0" apply false
 
     // Kotlin plugin
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    //id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 
     id("com.google.dagger.hilt.android") version "2.46.1" apply false
-    id("androidx.navigation.safeargs") version "2.5.3" apply false
+    id("androidx.navig ation.safeargs") version "2.5.3" apply false
 }
 
